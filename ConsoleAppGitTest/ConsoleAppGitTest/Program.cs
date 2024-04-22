4ffe6256c0c2d4c70a63a30295eac04b258926c5
linkomizin <linkomizin@gmail.com>
2024-04-22 23:01:32 +0300

    пуллинг

--- ConsoleAppGitTest/ConsoleAppGitTest/Program.cs
+++ ConsoleAppGitTest/ConsoleAppGitTest/Program.cs
@@ -7,4 +7,9 @@ static int GetSum(int a, int b)
 {
     return a + b + 4;
 }
+<<<<<<< HEAD
 Console.WriteLine("Hello, World, MOUSE, 2");
+=======
+Console.WriteLine("Hello, World, Test, 2");
+MOUSE
+>>>>>>> 0a07fcb25e55e7b957d658a3b08208cc888a3278
